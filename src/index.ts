b3eec9ab63657cda2ce57a69501bbc84253275f4
matheuspolachini <matheuspolachini@outlook.com>
2021-03-28 12:34:44 -0300

    Add event listener method to user model

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Matheus", age: 21 });
 
-user.set({ name: "Math" });
+user.on("change", () => {});
+user.on("change", () => {});
+user.on("create", () => {});
 
-console.log(`Name: ${user.get("name")}`);
-console.log(`Age: ${user.get("age")}`);
+console.log(user);
