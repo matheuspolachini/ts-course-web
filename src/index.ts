32ad0b7f8505ef3da404f11a11171497becb1a4a
matheuspolachini <matheuspolachini@outlook.com>
2021-03-28 11:57:42 -0300

    Add user model

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello World");
+import { User } from "./models/User";
+
+const user = new User({ name: "Matheus", age: 21 });
+
+console.log(`Name: ${user.get("name")}`);
+console.log(`Age: ${user.get("age")}`);
