6463166de191050e6ba46d116d34977643b663dd
matheuspolachini <matheuspolachini@outlook.com>
2021-03-28 12:07:17 -0300

    Add set method to user model

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Matheus", age: 21 });
 
+user.set({ name: "Math" });
+
 console.log(`Name: ${user.get("name")}`);
 console.log(`Age: ${user.get("age")}`);
