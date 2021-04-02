7427f5f1d140695a55773be8b37d744a0913ace9
matheuspolachini <matheuspolachini@outlook.com>
2021-04-02 16:06:43 -0300

    Extract fetch and save into a separate class

--- src/models/User.ts
+++ src/models/User.ts
@@ -19,23 +19,4 @@ export class User {
   set(update: UserProps): void {
     Object.assign(this.data, update);
   }
-
-  fetch(): void {
-    const id = this.get("id");
-    axios
-      .get(`http://localhost:3000/users/${id}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get("id");
-
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`);
-    } else {
-      axios.post("http://localhost:3000/users", this.data);
-    }
-  }
 }
