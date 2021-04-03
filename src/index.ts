398846478463c4d2c62a279263c19b81369fe13f
matheuspolachini <matheuspolachini@outlook.com>
2021-04-02 22:39:02 -0300

    Re-add save method into user class

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { User } from "./models/User";
 
 const user: User = new User({
   id: 1,
+  name: "newer name",
+  age: 0,
 });
 
-user.on("change", () => {
+user.on("save", () => {
   console.log(user);
 });
 
-user.fetch();
+user.save();
