ff5db78e0d4033fe14b256c70047e2c703de824f
matheuspolachini <matheuspolachini@outlook.com>
2021-04-07 22:20:07 -0300

    Add event handling to UserForm

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -6,13 +6,43 @@ export class UserForm {
       <div>
         <h1>User Form</h1>
         <input />
+        <button>Click Me</button>
       </div>
     `;
+  }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      "click:button": this.onButtonClick,
+      "mouseover:h1": this.onHeaderOver,
+    };
+  }
+
+  onButtonClick(): void {
+    console.log("Hi there");
+  }
+
+  onHeaderOver(): void {
+    console.log("H1 was hovered over");
+  }
+
+  bindEvents(fragment: DocumentFragment) {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 
   render(): void {
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
