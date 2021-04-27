0927a97e725bee4fa2691be93e9abdaaa3b110ea
matheuspolachini <matheuspolachini@outlook.com>
2021-04-27 19:48:28 -0300

    Remove user info display

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -4,9 +4,6 @@ export class UserForm extends View<User, UserProps> {
   template(): string {
     return `
       <div>
-        <h1>User Form</h1>
-        <div>User name: ${this.model.get("name")}</div>
-        <div>User age: ${this.model.get("age")}</div>
         <input />
         <button class="set-name">Change Name</button>
         <button class="set-age">Set Random Age</button>
